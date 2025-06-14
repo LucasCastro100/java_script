a6725810ae5f59ffceb302b8aaa0e4919caea193
LucasCastro100 <lucascastro121295@gmail.com>
2025-06-14 13:16:45 -0300

    subindo alterações com js

---
+++ react/first-next/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="text-xl font-semibold text-gray-800">Olá, mundo</p>
+    </div>
+  );
+}
