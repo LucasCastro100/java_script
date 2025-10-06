3bd82fb56e88f9b24f72a4b36f0a8d2e03a68e0c
LucasCastro100 <lucascastro121295@gmail.com>
2025-10-06 18:05:08 -0300

    evento de envio pelo formEvent apra outra pagina com parametro e botao ver mais

--- react/cripto-moedas/src/components/Header.tsx
+++ react/cripto-moedas/src/components/Header.tsx
@@ -3,7 +3,7 @@ import logoImg from "../assets/logo.svg";
 
 export function Header() {
     return (
-        <div className="w-full bg-gray-600 px-4 py-4 md:py-8 ">
+        <div className="w-full px-4 py-4 md:py-8">
             <Link to="/">
                 <img src={logoImg} alt="Logo Cripto Moedas" className="mx-auto" />
             </Link>
--- react/cripto-moedas/src/pages/Home/index.tsx
+++ react/cripto-moedas/src/pages/Home/index.tsx
@@ -1,71 +1,77 @@
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
 import { BsSearch } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export function Home() {
-  const [cripto, setCripto] = useState('')
-  async function handleForm(e: any) {
-    e.preventDefault()
+  const [input, SetInput] = useState('')
+  const navigate = useNavigate()
+  const value: number = -1.20
 
-    alert(cripto)
-    setCripto('')
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+    if (input === '') return
+
+    navigate(`/detail/${input}`)    
   }
 
   return (
-    <div className="p-4 mt-4">
-      <form className="w-full flex flex-row gap-4">
+    <div className="p-4">
+      <form className="w-full flex flex-row gap-4" onSubmit={handleSubmit}>
         <input
           size={30}
           type="text"
-          placeholder="Digite a Cripto Moeda"
+          placeholder="Digite o nome de uma moeda digital EX: bitcoin"
           className="flex-1 bg-white text-black px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          value={cripto}
-          onChange={(e) => { setCripto(e.target.value) }}
+          value={input}
+          onChange={(e) => { SetInput(e.target.value) }}
         />
 
-        <button type="submit" className='flex items-center justify-center border-blue-500 bg-white rounded-md p-2' onClick={handleForm}>
+        <button type="submit" className='flex items-center justify-center border-blue-500 bg-white rounded-md p-2'>
           <BsSearch size={20} color='#000' />
         </button>
       </form>
 
-      <table className='mt-4 w-full text-white'>
-        <thead>
-          <tr>
-            <th scope='col'>MOEDA</th>
-            <th scope='col'>VALOR MERCADO</th>
-            <th scope='col'>PREÇO</th>
-            <th scope='col'>VOLUME</th>
-            <th scope='col'>MUDANÇA 24H</th>
-          </tr>
-        </thead>
+      <div className="w-full overflow-x-auto mt-4">
+        <table className="w-full min-w-max border-collapse border border-gray-300 text-center">
+          <thead className="bg-gray-100">
+            <tr className='rounded-md'>
+              <th scope='col' className="px-4 py-2 font-bold">MOEDA</th>
+              <th scope='col' className="px-4 py-2 font-bold">VALOR MERCADO</th>
+              <th scope='col' className="px-4 py-2 font-bold">PREÇO</th>
+              <th scope='col' className="px-4 py-2 font-bold">VOLUME</th>
+              <th scope='col' className="px-4 py-2 font-bold">MUDANÇA 24H</th>
+            </tr>
+          </thead>
 
-        <tbody id='tbody'>
-          <tr>
-            <td data-label="Moeda">
-              <Link to="/detail/bitcoin" className='flex flex-row items-center gap-2'>
-                <span>Bitcoin</span>
-                <span className='uppercase text-gray-500'>BTC</span>
-              </Link>
-            </td>
+          <tbody id='tbody' className='bg-gray-100'>
+            <tr>
+              <td data-label="Moeda" className="px-4 py-2 font-bold">
+                <Link to="/detail/bitcoin" className='flex flex-row items-center gap-2 justify-center'>
+                  <span>Bitcoin</span>
+                  <span className='uppercase text-gray-500'>BTC</span>
+                </Link>
+              </td>
 
-            <td data-label="Valor Mercado">
-              1T
-            </td>
+              <td data-label="Valor Mercado" className="px-4 py-2 font-bold">
+                1T
+              </td>
 
-            <td data-label="Preco">
-              8.000
-            </td>
+              <td data-label="Preco" className="px-4 py-2 font-bold">
+                8.000
+              </td>
 
-            <td data-label="Volume">
-              500B
-            </td>
+              <td data-label="Volume" className="px-4 py-2 font-bold">
+                500B
+              </td>
 
-            <td data-label="Mudanca">
-              1.20
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              {/* aqui se for positivo fica verde se nao fica vermelho */}
+              <td data-label="Mudanca"  className={`px-4 py-2 font-bold ${value > 0 ? 'text-green-600' : 'text-red-600'}`}>
+                {value}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
