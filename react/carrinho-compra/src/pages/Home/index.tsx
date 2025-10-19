5929c7a0e188200d7d2ccb68f2b41214e1bf441e
LucasCastro100 <lucascastro121295@gmail.com>
2025-10-19 18:14:40 -0300

    Iniciando estudo com projeto carrinho de compra

--- react/carrinho-compra/src/pages/Home/index.tsx
+++ react/carrinho-compra/src/pages/Home/index.tsx
@@ -9,6 +9,10 @@ export function Home() {
             <ToastContainer />
             <h1 className="text-center text-white">Home Page {cartItems}</h1>
 
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+
+            </div>
+
 
             {/* <button onClick={addToCart} className="text-white">Adicionar</button> */}
         </div>
