cf5ff0f29882ed5e7429777f82f0a6d218b4db43
LucasCastro100 <lucascastro121295@gmail.com>
2025-11-28 09:12:11 -0300

    Sistema de tarefas

--- next_js/tarefas/src/app/(site)/page.tsx
+++ next_js/tarefas/src/app/(site)/page.tsx
@@ -1,5 +1,19 @@
-export default function Homepage() {
+import Image from "next/image";
+
+import heroImg from '../../../public/assets/hero.png'
+
+export default function HomePage() {
     return (
-        <h1 className="font-bold text-3xl">Homepage</h1>
+        <div className="flex-1 flex items-center justify-center">
+            <div className="flex flex-col gap-8 md:gap-12 max-w-[500px]">
+                <Image
+                    alt="Logo tarefas+"
+                    src={heroImg}
+                    className="w-full"
+                />
+
+                <p className="text-center font-medium text-3xl">Sistema feito apra você organizar seus estudos e tarefas</p>
+            </div>
+        </div>
     );
 }
---
+++ next_js/tarefas/src/app/component/header/index.tsx
@@ -0,0 +1,9 @@
+export function Header(){
+    return(
+        <div className="w-full border-b-4 border-b-white">
+            <div className="w-full max-w-6xl p-4 m-auto">
+                <h1 className="font-bold text-3xl text-center">Tarefa + | Organize suas tarefas de forma fácil</h1>
+            </div>
+        </div>
+    )
+}
